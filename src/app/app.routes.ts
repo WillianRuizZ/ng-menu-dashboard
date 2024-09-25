20e1db79a2b64e3a8b2f449b9e0031f8721fa61e
willian ruiz <122157786+willianRuizCSS@users.noreply.github.com>
2024-09-25 15:25:30 -0500

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./shared/components/layout/layout.component'),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./busines/dashboard/dashboard.component')
+      },
+      {
+        path: 'profile',
+        loadComponent: () => import('./busines/profile/profile.component')
+      },
+      {
+        path: 'tables',
+        loadComponent: () => import('./busines/tables/tables.component')
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      }
+    ]
+  }
+
+];
